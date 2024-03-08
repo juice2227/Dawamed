82319d81d5c936f19b3df52ad89ed4227da3d276
Jobwafula <jobwafulabg@gmail.com>
2024-03-08 17:49:43 +0300

    mongo connect

--- server/index.js
+++ server/index.js
@@ -2,27 +2,24 @@ const express  = require('express');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT
+const mongoose = require('mongoose')
+
+
+const uri = process.env.MONGO_URI
+async function connect() {
+    try {
+      await mongoose.connect(uri, {useNewUrlParser: true,
+        useUnifiedTopology: true, });
+      console.log("Connected to MongoDB");
+    } catch (error) {
+      console.error("Error connecting to MongoDB:", error.message);
+      process.exit(1); // Exit the process in case of an error
+    }
+  }
+  
+  connect();
 
 
 app.listen(port,()=>{
     console.log(`Server listening to port ${port} `)
 })
-
-
-
-
-const mongoose = require('mongoose');
-
-
-const mongoURI = 'mongodb://localhost:27017/DawaMed'; // Replace 'mydatabase' with your database nam
-
-try{
-    mongoose.connect(mongoURI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      })
-      console.log('MongoDB connected successfully');
-}
-    catch(error) {
-  console.error('MongoDB connection error:', err);
-};
